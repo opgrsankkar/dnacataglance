964c204bcd2ff7e5421ee4249932fef535e3ef85
Sankkara Narayanan Ranganathan <sankkr@cisco.com>
2019-12-26 18:34:12 +0530

    change to confirm with user before starting proxy config
    change internet test url from google.com to cxd.cisco.com
    
    change ls -l to ls -lht on list_rca_files
    to enable human readable file sizes and
    sort on timestamp

--- upload_rca.sh
+++ upload_rca.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 
-function list_rca_files(){
-	echo ''''''''''''''''''''''''''''''''''''''''''''''
-        echo These are the rca files available:
-        ls -l /data/rca
-	echo ''''''''''''''''''''''''''''''''''''''''''''''
+cxd_url=cxd.cisco.com
+test_port=443
+
+list_rca_files() {
+    echo ''''''''''''''''''''''''''''''''''''''''''''''
+    echo These are the rca files available:
+    ls -lht /data/rca
+    echo ''''''''''''''''''''''''''''''''''''''''''''''
 }
 
-echo "Enter Proxy settings. Hit Enter to proceed if field is Not Applicable"
-printf "\n"
+configure_https_proxy() {
+    echo "Configure HTTPS Proxy settings."
+    echo "Hit Enter to proceed if a field is Not Applicable"
+    echo ""
+    read -p    "Enter Proxy URL/IP      : " proxy_url
+    read -p    "Enter Proxy port number : " proxy_port
+    read -p    "Enter Proxy username    : " proxy_username
+    read -p -s "Enter Proxy password    : " proxy_password
+    echo ""
+    if [ ${#proxy_username} -le 1 ]; then
+        echo "Setting proxy. No proxy credentials. Proceeding";
+        export https_proxy=http://$proxy_url:$proxy_port/ ;
+    else
+        echo "Setting proxy with credentials. Proceeding";
+        export https_proxy=http://$proxy_username:$proxy_password@$proxy_url:$proxy_port/ ;
+    fi
+}
 
-read -p "Enter HTTPS Proxy URL/ IP : " proxy_url
-read -p "Enter Proxy port number : " proxy_port
-read -p "Enter Proxy username( if any): " proxy_username
-read -s  -p "Enter Proxy password( if any): " proxy_pwd
-printf "\n"
-
-if [ ${#proxy_url} -le 1 ]; 
-   then echo "No proxy set. Proceeding";
-elif [ ${#proxy_username} -le 1 ];
-   then echo "Setting proxy. No proxy credentials. Proceeding";
-   export https_proxy=http://$proxy_url:$proxy_port/ ;
+echo "Testing connectivity to $cxd_url:$test_port"
+if nc -zw1 $cxd_url $test_port; then
+    echo "Connection Successful";
 else
-   echo "Setting proxy with credentials. Proceeding";
-   export https_proxy=http://$proxy_username:$proxy_pwd@$proxy_url:$proxy_port/ ;
+    echo "Connection NOT Successful.";
+    read -n1 -p "Try configuring a HTTPS proxy [y/n]: " response
+    echo ""
+    case "$response" in
+        [yY])
+            configure_https_proxy
+            if nc -zw1 $cxd_url $test_port; then
+                echo "Connection Successful";
+            else
+                echo "Upload terminated";
+                exit 1;
+            fi
+        ;;
+
+        ?)
+            echo "Upload terminated"
+            exit 2;
+        ;;
+    esac
 fi
 
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 list_rca_files
 if /opt/maglev/bin/python _main.py; then
-   nohup /opt/maglev/bin/python uploadRca.py &
+    nohup /opt/maglev/bin/python uploadRca.py &
 fi
